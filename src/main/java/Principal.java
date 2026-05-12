5fbb4809d5c262c97cb034234111defe5da34c2f
Julia Dick <juliadickk9@gmail.com>
2026-05-12 20:13:42 -0300

    init: Início do projeto

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -37,10 +37,51 @@ public class Principal {
             switch (opcao) {
 
                 case 1:
+                    menuCadastro();
                     break;
 
                 case 0:
                     JOptionPane.showMessageDialog(null, "Sistema finalizado.");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+            }
+
+        } while (opcao != 0);
+        
+    }
+    
+       // TELA 1.1
+    public static void menuCadastro() {
+
+        int opcao;
+
+        do {
+
+            opcao = Integer.parseInt(
+                    JOptionPane.showInputDialog( "XYZ COMERCIO DE PRODUTOS LTDA.\n"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE\n"
+                            +"\n"
+                            + "CADASTRO DE PRODUTOS\n"
+                            +"\n"
+                            + "1 - INCLUSÃO\n"
+                            + "2 - ALTERAÇÃO\n"
+                            + "3 - CONSULTA\n"
+                            + "4 - EXCLUSÃO\n"
+                            + "0 - RETORNAR\n"
+                            +"\n"
+                            + "OPÇÃO: "
+                    )
+            );
+
+            switch (opcao) {
+
+                case 1:
+                    break;
+
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Retornando ao menu principal...");
                     break;
 
                 default:
